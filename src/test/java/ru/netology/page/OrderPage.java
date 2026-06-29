2d256d2887f001342fafba601ab62bf28240ec26
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-30 02:08:40 +0300

    Update OrderPage.java

--- src/test/java/ru/netology/page/OrderPage.java
+++ src/test/java/ru/netology/page/OrderPage.java
@@ -8,35 +8,41 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class OrderPage {
-    
+    // Более надежные селекторы на основе HTML
     private final SelenideElement cityField = $("[data-test-id='city'] input");
-    private final SelenideElement dateField = $("input[type='date']");
-    private final SelenideElement nameField = $("input[name='name']");
-    private final SelenideElement phoneField = $("input[name='phone']");
+    private final SelenideElement dateField = $(".calendar-input__native-control");
+    private final SelenideElement nameField = $("[name='name']");
+    private final SelenideElement phoneField = $("[name='phone']");
     private final SelenideElement agreementCheckbox = $("[data-test-id='agreement'] .checkbox__box");
     private final SelenideElement planButton = $(".button__text");
 
-    public OrderPage fillForm(DataHelper.UserInfo userInfo, String date) {
+    public void fillForm(DataHelper.UserInfo userInfo, String date) {
+    
+        sleep(1000);
+        
+     
         cityField.shouldBe(visible).setValue(userInfo.getCity());
         sleep(300);
+        
+      
         dateField.shouldBe(visible).setValue(date);
         sleep(300);
+        
+        
         nameField.shouldBe(visible).setValue(userInfo.getName());
         sleep(300);
+        
         phoneField.shouldBe(visible).setValue(userInfo.getPhone());
         sleep(300);
-        return this;
     }
 
-    public OrderPage agree() {
+    public void agree() {
         agreementCheckbox.shouldBe(visible).click();
         sleep(300);
-        return this;
     }
 
-    public OrderPage continueOrder() {
+    public void continueOrder() {
         planButton.shouldBe(visible).click();
         sleep(500);
-        return this;
     }
 }
