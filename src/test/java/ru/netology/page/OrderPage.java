995d2af9b325a86c8bd563a9580ec116dacd63a8
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-30 02:04:30 +0300

    Update OrderPage.java

--- src/test/java/ru/netology/page/OrderPage.java
+++ src/test/java/ru/netology/page/OrderPage.java
@@ -5,30 +5,38 @@ import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class OrderPage {
+    
     private final SelenideElement cityField = $("[data-test-id='city'] input");
-    private final SelenideElement dateField = $(".calendar-input__native-control");
-    private final SelenideElement nameField = $("[name='name']");
-    private final SelenideElement phoneField = $("[name='phone']");
+    private final SelenideElement dateField = $("input[type='date']");
+    private final SelenideElement nameField = $("input[name='name']");
+    private final SelenideElement phoneField = $("input[name='phone']");
     private final SelenideElement agreementCheckbox = $("[data-test-id='agreement'] .checkbox__box");
-    private final SelenideElement planButton = $(".button__text"); // Кнопка "Запланировать"
+    private final SelenideElement planButton = $(".button__text");
 
     public OrderPage fillForm(DataHelper.UserInfo userInfo, String date) {
         cityField.shouldBe(visible).setValue(userInfo.getCity());
+        sleep(300);
         dateField.shouldBe(visible).setValue(date);
+        sleep(300);
         nameField.shouldBe(visible).setValue(userInfo.getName());
+        sleep(300);
         phoneField.shouldBe(visible).setValue(userInfo.getPhone());
+        sleep(300);
         return this;
     }
 
     public OrderPage agree() {
         agreementCheckbox.shouldBe(visible).click();
+        sleep(300);
         return this;
     }
 
     public OrderPage continueOrder() {
         planButton.shouldBe(visible).click();
+        sleep(500);
         return this;
     }
 }
