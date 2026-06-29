071f8a2ec8cb8e7239ade142750a5659fdd96404
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-30 02:25:59 +0300

    Update SuccessPage.java

--- src/test/java/ru/netology/page/SuccessPage.java
+++ src/test/java/ru/netology/page/SuccessPage.java
@@ -4,19 +4,11 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
 
 public class SuccessPage {
     private final SelenideElement successNotification = $("[data-test-id='order-success']");
 
-    public SuccessPage checkSuccessVisible() {
+    public void checkSuccessVisible() {
         successNotification.shouldBe(visible);
-        sleep(500);
-        return this;
-    }
-
-    public SuccessPage checkSuccessText(String expectedText) {
-        successNotification.shouldBe(visible);
-        return this;
     }
 }
