2d183bde926c33da157c38873e01cd3dd655bce4
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-30 01:00:06 +0300

    Update OrderPage.java

--- src/test/java/ru/netology/page/OrderPage.java
+++ src/test/java/ru/netology/page/OrderPage.java
@@ -3,32 +3,32 @@ package ru.netology.page;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class OrderPage {
     private final SelenideElement cityField = $("[data-test-id='city'] input");
-    private final SelenideElement dateField = $("[data-test-id='date'] input");
-    private final SelenideElement nameField = $("[data-test-id='name'] input");
-    private final SelenideElement phoneField = $("[data-test-id='phone'] input");
+    private final SelenideElement dateField = $(".calendar-input__native-control");
+    private final SelenideElement nameField = $("[name='name']");
+    private final SelenideElement phoneField = $("[name='phone']");
     private final SelenideElement agreementCheckbox = $("[data-test-id='agreement'] .checkbox__box");
-    private final SelenideElement continueButton = $(".button_theme_alfa-on-white");
+    private final SelenideElement planButton = $(".button__text"); // Кнопка "Запланировать"
 
     public OrderPage fillForm(DataHelper.UserInfo userInfo, String date) {
-        cityField.setValue(userInfo.getCity());
-        dateField.setValue(date);
-        nameField.setValue(userInfo.getName());
-        phoneField.setValue(userInfo.getPhone());
+        cityField.shouldBe(visible).setValue(userInfo.getCity());
+        dateField.shouldBe(visible).setValue(date);
+        nameField.shouldBe(visible).setValue(userInfo.getName());
+        phoneField.shouldBe(visible).setValue(userInfo.getPhone());
         return this;
     }
 
     public OrderPage agree() {
-        agreementCheckbox.click();
+        agreementCheckbox.shouldBe(visible).click();
         return this;
     }
 
     public OrderPage continueOrder() {
-        continueButton.click();
+        planButton.shouldBe(visible).click();
         return this;
     }
 }
