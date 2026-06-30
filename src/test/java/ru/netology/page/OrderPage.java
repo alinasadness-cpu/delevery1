b93a01cb1dfba97eb79537db8990160674dc1bfd
alinasadness-cpu <alina_sadness@inbox.ru>
2026-06-30 19:24:39 +0300

    Update OrderPage.java

--- src/test/java/ru/netology/page/OrderPage.java
+++ src/test/java/ru/netology/page/OrderPage.java
@@ -3,39 +3,50 @@ package ru.netology.page;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
 
 public class OrderPage {
+  
     private final SelenideElement cityField = $("[data-test-id='city'] input");
-    private final SelenideElement dateField = $(".calendar-input__native-control");
-    private final SelenideElement nameField = $("input[name='name']");
-    private final SelenideElement phoneField = $("input[name='phone']");
+    private final SelenideElement dateField = $("[data-test-id='date'] input");
+    private final SelenideElement nameField = $("[data-test-id='name'] input");
+    private final SelenideElement phoneField = $("[data-test-id='phone'] input");
     private final SelenideElement agreementCheckbox = $("[data-test-id='agreement'] .checkbox__box");
     private final SelenideElement planButton = $(".button__text");
 
+   
+    private final SelenideElement successNotification = $("[data-test-id='order-success']");
+    private final SelenideElement replanNotification = $("[data-test-id='replan-notification']");
+    private final SelenideElement replanButton = $("[data-test-id='replan-notification'] button");
+
     public void fillForm(DataHelper.UserInfo userInfo, String date) {
-        cityField.shouldBe(visible).setValue(userInfo.getCity());
-        sleep(300);
-        
-        dateField.shouldBe(visible).setValue(date);
-        sleep(300);
-        
-        nameField.shouldBe(visible).setValue(userInfo.getName());
-        sleep(300);
-        
-        phoneField.shouldBe(visible).setValue(userInfo.getPhone());
-        sleep(300);
+        cityField.setValue(userInfo.getCity());
+        dateField.setValue(date);
+        nameField.setValue(userInfo.getName());
+        phoneField.setValue(userInfo.getPhone());
     }
 
     public void agree() {
-        agreementCheckbox.shouldBe(visible).click();
-        sleep(300);
+        agreementCheckbox.click();
     }
 
     public void continueOrder() {
-        planButton.shouldBe(visible).click();
-        sleep(500);
+        planButton.click();
+    }
+
+    public void replan() {
+        replanButton.click();
+    }
+
+    public void checkSuccessNotification(String expectedText) {
+        successNotification.shouldBe(visible)
+                .shouldHave(text(expectedText));
+    }
+
+    public void checkReplanNotification(String expectedText) {
+        replanNotification.shouldBe(visible)
+                .shouldHave(text(expectedText));
     }
 }
