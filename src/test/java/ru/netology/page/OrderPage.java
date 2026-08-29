1f1c5627f3c9e8aac1c47c069a994702c35969a5
unknown <alina_sadness@inbox.ru>
2026-08-29 17:39:37 +0300

    Добавил проект с Allure

---
+++ src/test/java/ru/netology/page/OrderPage.java
@@ -0,0 +1,60 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class OrderPage {
+
+    private final SelenideElement cityField = $("[data-test-id='city'] input");
+    private final SelenideElement dateField = $("[data-test-id='date'] input");
+    private final SelenideElement nameField = $("[data-test-id='name'] input");
+    private final SelenideElement phoneField = $("[data-test-id='phone'] input");
+    private final SelenideElement agreementCheckbox = $("[data-test-id='agreement'] .checkbox__box");
+    private final SelenideElement planButton = $(".button__text");
+
+    private final SelenideElement successNotification = $("[data-test-id='success-notification'] .notification__content");
+    private final SelenideElement replanNotification = $("[data-test-id='replan-notification'] .notification__content");
+    private final SelenideElement replanButton = $("[data-test-id='replan-notification'] button");
+
+    public void fillForm(DataHelper.UserInfo userInfo, String date) {
+
+        cityField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        cityField.setValue(userInfo.getCity());
+
+        dateField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        dateField.setValue(date);
+
+        nameField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        nameField.setValue(userInfo.getName());
+
+        phoneField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        phoneField.setValue(userInfo.getPhone());
+    }
+
+    public void agree() {
+        agreementCheckbox.click();
+    }
+
+    public void continueOrder() {
+        planButton.click();
+    }
+
+    public void replan() {
+        replanButton.click();
+    }
+
+    public void checkSuccessNotification(String expectedText) {
+        successNotification.shouldBe(visible)
+                .shouldHave(text(expectedText));
+    }
+
+    public void checkReplanNotification(String expectedText) {
+        replanNotification.shouldBe(visible)
+                .shouldHave(text(expectedText));
+    }
+}
---
+++ src/test/java/ru/netology/tests/OrderDeliveryTest.java
@@ -0,0 +1,40 @@
+package ru.netology.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.OrderPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderDeliveryTest {
+    private DataHelper.UserInfo userInfo;
+    private String initialDate;
+    private String newDate;
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+        userInfo = DataHelper.generateUserInfo();
+        initialDate = DataHelper.generateDate(3);
+        newDate = DataHelper.generateDate(7);
+    }
+
+    @Test
+    void shouldReplanMeeting() {
+        OrderPage orderPage = new OrderPage();
+
+        orderPage.fillForm(userInfo, initialDate);
+        orderPage.agree();
+        orderPage.continueOrder();
+        orderPage.checkSuccessNotification("Встреча успешно запланирована на " + initialDate);
+
+        orderPage.fillForm(userInfo, newDate);
+        orderPage.continueOrder();
+
+        orderPage.checkReplanNotification("У вас уже запланирована встреча на другую дату. Перепланировать?");
+        orderPage.replan();
+
+        orderPage.checkSuccessNotification("Встреча успешно запланирована на " + newDate);
+    }
+}
